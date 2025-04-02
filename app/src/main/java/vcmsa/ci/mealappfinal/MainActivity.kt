89320841857999395b0f8c527199b3fbc76279c0
Bayanda-Moabelo <ST10485809@vcconnect.edu.za>
2025-04-02 21:42:03 +0200

    I added and declared my variables

--- app/src/main/java/vcmsa/ci/mealappfinal/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/mealappfinal/MainActivity.kt
@@ -1,20 +1,27 @@
 package vcmsa.ci.mealappfinal
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.Display
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()  {
+
+    private lateinit var periodInputField: EditText
+    private lateinit var recommendationDisplay: TextView
+    private lateinit var getRecommendationBtn: Button
+    private lateinit var clearFieldsBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        initializeComponents()
+        setupInteractions()
+
     }
-}
+
